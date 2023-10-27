b73982d3c652eb827b879cef558efae14d18a299
Bradley <bradleyandrewercole28@gmail.com>
2023-10-27 09:45:04 +0700

    Menghilangkan import penulisan redux lama

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-// Penulisan lama
-// import oldTodoSlice from "./reducers/oldTodoReducer"
-// Penulisan baru
 import todoSlice from "./slices/todoSlice";
 
 const store = configureStore({
