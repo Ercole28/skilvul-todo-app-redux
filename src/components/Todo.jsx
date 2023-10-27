0ffebda2a6b8afd8c74888e4e2856911d1c1a71d
Bradley <bradleyandrewercole28@gmail.com>
2023-10-27 12:01:10 +0700

    Adjusting Tombol Todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -30,21 +30,18 @@ export default function Todo(props) {
         key={id}
         className="w-full flex items-center justify-between border border-slate-100/50 px-3 py-2 rounded-lg mb-2 hover:translate-x-2 duration-300 ease-out"
       >
-        <div className="flex gap-x-2 cursor-default">
-          <span>{completed ? "✅" : "⛔"}</span>
-          <span className={`${completed ? "line-through" : ""} text-white`}>{text}</span>
-        </div>
-        <div className="flex gap-x-2">
-          <button
+        <div className="flex gap-x-2 items-center">
+          <button 
             onClick={() => toggleTodo(id)}
-            className="flex justify-center items-center bg-sky-500 w-10 h-8 rounded-md"
+            className="flex justify-center items-center w-6 h-6 border border-slate-100/50 rounded-sm cursor-pointer"
           >
-            {!completed ? (
-              <FontAwesomeIcon icon={faCheck} color={"white"} />
-            ) : (
-              <FontAwesomeIcon icon={faXmark} color={"white"} />
+            {completed && (
+              <FontAwesomeIcon icon={faCheck} className="text-lg text-green-400" />
             )}
           </button>
+          <span className={`${completed ? "line-through" : ""} text-white cursor-default`}>{text}</span>
+        </div>
+        <div className="flex gap-x-2">
           <button
             onClick={toggleModal}
             className="flex justify-center items-center bg-amber-400 w-10 h-8 rounded-md"
