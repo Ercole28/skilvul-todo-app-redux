a7ca6f0f0ca8336b8d7362bf48eebb7a91e49e12
Bradley <bradleyandrewercole28@gmail.com>
2023-10-27 11:50:55 +0700

    Memecah Input & Setup Filter Todo

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,82 +1,71 @@
-import { useState } from "react"
-import { addTask } from "../redux/slices/todoSlice"
-import { useSelector, useDispatch } from "react-redux"
-import TodosList from "./lists/TodosList"
-import Alert from "./alerts/Alert"
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import TodosList from "./lists/TodosList";
+import Alert from "./alerts/Alert";
+import InputTodo from "./InputTodo";
 
-export default function NewRedux(){
-  const [todo, setTodo] = useState('')
-  const [filter, setFilter] = useState("all"); 
-  const [alert, setAlert] = useState(false)
-  
-  const todos = useSelector((state) => state.todo.tasks)
-  const dispatch = useDispatch()
+export default function TodoApp() {
+  const [filter, setFilter] = useState("all");
+  const todos = useSelector((state) => state.todo.tasks);
 
-  const handleChange = (e) => {
-    setTodo(e.target.value)
-  }
-
-  const addTodo = (e) => {
-    e.preventDefault()
-
-    if (todo.trim() !== "") {
-      dispatch(addTask(todo));
-      setTodo('');
-      setAlert(false);
-    } else {
-      setAlert(true);
-    }
-  }
-
-  const handleFilterChange = (e) => {
-    setFilter(e.target.value);
+  const handleFilterChange = (newFilter) => {
+    setFilter(newFilter);
   };
 
   const filteredTodos = todos.filter((todo) => {
     if (filter === "all") {
       return todo;
-    } else if (filter === "true") {
+    } else if (filter === "completed") {
       return todo.completed;
-    } else if (filter === "false") {
+    } else if (filter === "active") {
       return !todo.completed;
     }
     return todo;
   });
 
-  return(
+  return (
     <div className="w-full">
-      <h1 className="font-bold text-xl text-white mb-1 -ml-1">⭐ New Redux</h1>
-      <form className="w-full grid grid-cols-4 gap-x-3 mb-3">
-        <input
-          type="text" 
-          placeholder="Input your to do" 
-          value={todo}
-          onChange={handleChange}
-          className="w-full text-black col-span-2 px-4 py-2.5 rounded-md focus:outline-none placeholder:text-slate-500"
-        />
-        <select 
-          value={filter}
-          onChange={handleFilterChange}
-          className="w-full text-slate-500 px-4 py-2.5 rounded-md focus:outline-none appearance-none"
-        >
-          <option value="all">All</option>
-          <option value="true">Done</option>
-          <option value="false">Not Done</option>
-        </select>
-        <button onClick={addTodo} className="font-bold bg-slate-600 px-10 py-2 rounded-md text-white">Add</button>
-      </form>
+      <h1 className="font-bold text-sm text-white mb-1 -ml-1">⭐ New Redux</h1>
+      <InputTodo/>
 
-      {alert && (
-        <p className="text-center text-red-500 mb-3">
-          Fill out the input field first! ✍️
-        </p>
-      )}
+      <div className="flex gap-x-4 mb-5">
+        <button
+          onClick={() => handleFilterChange('all')}
+          className={`${
+            filter === "all"
+              ? "bg-sky-500 text-white"
+              : "bg-slate-500 text-white"
+          } px-3 py-1 rounded-md duration-200`}
+        >
+          All
+        </button>
+        <button
+          onClick={() => handleFilterChange("active")}
+          className={`${
+            filter === "active"
+              ? "bg-sky-500 text-white"
+              : "bg-slate-500 text-white"
+          } px-3 py-1 rounded-md duration-200`}
+        >
+          Active
+        </button>
+        <button
+          onClick={() => handleFilterChange("completed")}
+          className={`${
+            filter === "completed"
+              ? "bg-sky-500 text-white"
+              : "bg-slate-500 text-white"
+          } px-3 py-1 rounded-md duration-200`}
+        >
+          Completed
+        </button>
+      </div>
 
       {todos.length !== 0 ? (
-        <TodosList todosData={filteredTodos}/>
+        <TodosList todosData={filteredTodos} />
       ) : (
-        <Alert message={'Make your first to do 🙌'}/>
+        <Alert message={"Make your first to do 🙌"} />
       )}
     </div>
-  )
+  );
 }
