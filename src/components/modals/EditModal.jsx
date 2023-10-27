9b19bcc20b7dd30a362ddd86b87a3cb712bc9484
Bradley <bradleyandrewercole28@gmail.com>
2023-10-27 11:50:22 +0700

    Memperbaharui Edit Modal

--- src/components/modals/EditModal.jsx
+++ src/components/modals/EditModal.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { updateTask } from "../../redux/slices/todoSlice"
 import { useDispatch } from "react-redux"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,19 +7,20 @@ import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function EditModal(props) {
   const { id, text, toggleModalFunc } = props;
-
-  const dispatch = useDispatch();
-
-  const updateTodo = (e) => {
-    e.preventDefault();    
-    dispatch(updateTask(newData));
-    toggleModalFunc();
-  };
-
   const [newData, setNewData] = useState({
     id: id,
     text: text,
   });
+  const inputRef = useRef(null);
+
+  const dispatch = useDispatch();
+
+  const updateTodo = (e) => {
+    e.preventDefault();
+    
+    dispatch(updateTask(newData));
+    toggleModalFunc();
+  };
 
   const handleEditForm = (e) => {
     setNewData({
@@ -28,31 +29,35 @@ export default function EditModal(props) {
     });
   };
 
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []) 
+
   return (
-    <div className="bg-black/70 fixed w-full h-screen top-0 left-0 flex justify-center items-center duration-3">
-      <form className="relative w-full max-w-md bg-[#181818] p-8 rounded-xl">
+    <div className="bg-black/70 fixed w-full h-screen top-0 left-0 px-4 flex justify-center items-center duration-300">
+      <form className="relative w-full max-w-md bg-[#181818] p-6 sm:p-8 rounded-xl">
         <h1 className="text-xl font-bold text-white">Edit To Do</h1>
         <input
+          ref={inputRef}
           type="text"
+          placeholder="Edit your todo"
           className="w-full text-black col-span-2 px-4 py-2.5 rounded-md focus:outline-none placeholder:text-slate-500 mb-3"
           value={newData.text}
           onChange={handleEditForm}
         />
         <button
           onClick={updateTodo}
-          type="button"
-          className="w-full font-bold bg-slate-600 px-10 py-3 rounded-md text-white"
+          className="w-full font-bold bg-sky-500 px-10 py-3 rounded-md text-white"
         >
           Edit
         </button>
 
-        <button
+        <span
           onClick={toggleModalFunc}
-          type="button"
-          className="absolute top-4 right-4 text-white"
+          className="absolute top-4 right-4 text-white cursor-pointer"
         >
           <FontAwesomeIcon icon={faXmark} color={"white"} className="text-xl" />
-        </button>
+        </span>
       </form>
     </div>
   );
