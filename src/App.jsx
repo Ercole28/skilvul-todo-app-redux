d596a0b419a9160daa4746998e1e2117fd775dbc
Bradley <bradleyandrewercole28@gmail.com>
2023-10-27 09:42:43 +0700

    Menganti judul

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="w-full h-screen max-w-xl flex flex-col items-center mx-auto py-10">
       <h1 className="font-bold text-3xl text-white mb-5">
-        <span className="text-amber-400">Todo App</span> with <span className="text-purple-500">Redux 📝</span> 
+        <span className="text-amber-400">What&apos;s</span> the plan for <span className="text-sky-400">Today 👋</span> 
       </h1>
       <TodoApp/>
     </div>
