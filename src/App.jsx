026b8bb2d011324db6dbe54e6a92b576d1769f1f
Bradley <bradleyandrewercole28@gmail.com>
2023-10-27 09:47:05 +0700

    Menambahkan Padding X pada Container

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import TodoApp from "./components/TodoApp";
 
 export default function App() {
   return (
-    <div className="w-full h-screen max-w-xl flex flex-col items-center mx-auto pt-16 pb-8 gap-y-8">
-      <h1 className="font-bold text-3xl text-white">
+    <div className="w-full h-screen max-w-xl flex flex-col items-center mx-auto pt-16 pb-8 gap-y-8 px-6">
+      <h1 className="font-bold text-3xl text-white text-center">
         <span className="text-amber-400">What&apos;s</span> the plan for <span className="text-sky-400">Today 👋</span> 
       </h1>
       <TodoApp/>
