05a2ac20f0e0202307a8838b08c7affe54e3b4dc
Bradley <bradleyandrewercole28@gmail.com>
2023-10-27 09:37:59 +0700

    Memperbaharui Comp Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-// import { useState } from "react";
-// import OldRedux from "./components/OldRedux";
-import NewRedux from "./components/NewRedux";
+import TodoApp from "./components/TodoApp";
 
 export default function App() {
   return (
     <div className="w-full h-screen max-w-xl flex flex-col items-center mx-auto py-10">
       <h1 className="font-bold text-3xl text-white mb-5">
-        <span className="text-amber-400">Todo App</span> with <span className="text-purple-500">Redux</span> 
+        <span className="text-amber-400">Todo App</span> with <span className="text-purple-500">Redux 📝</span> 
       </h1>
-      {/* <OldRedux/> */}
-      <NewRedux/>
+      <TodoApp/>
     </div>
   );
 }
